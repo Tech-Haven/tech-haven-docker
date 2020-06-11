7b6f6970c52eb1ca5682a97505a5d71d9b6f7cee
Slightly Nervous Doggo <github@slightlynervousdoggo.com>
2020-06-11 09:41:46 -0400

    Got rid of webhooks.. Modify deploy script to check for thetechhaven.com repo Fixed NGINX HTTPS config

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
-cd thetechhaven.com
+#!/bin/sh
 
-echo "Pulling from Master"
-
-git pull origin Master
-
-echo "Pulled successfully from master"
+if [ ! -d "thetechhaven.com"  ]; then
+  echo "Cloning repo"
+  git clone https://github.com/Tech-Haven/thetechhaven.com.git
+else
+  echo "Pulling from master"
+  git -C thetechhaven.com/  pull origin master
+fi
 
 echo "Restarting server..."
 
 docker-compose down
-docker-compose build --no-cache
-docker-compose up -d
+docker image rm tech-haven-docker_api:latest
+docker volume rm tech-haven-docker_nodejs
+docker-compose up -d --build
 
 echo "Server restarted successfully"
